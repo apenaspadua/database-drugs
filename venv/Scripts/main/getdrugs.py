0bb61c926975d4c32a9f4a431b591e7c95151b1c
Matheus <padua.matheusdev@gmail.com>
2020-03-22 11:28:27 -0300

    printando dados

--- venv/Scripts/main/getdrugs.py
+++ venv/Scripts/main/getdrugs.py
@@ -1,27 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 chrome = webdriver.Chrome()
 chrome.get('https://reference.medscape.com/drug-interactionchecker')
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-           'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-item_list = chrome.find_elements_by_class_name('MDICilfulllist')
-
+array_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',  'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 search_field = chrome.find_element_by_xpath("//input[@id='MDICtextbox']")
-for alph in alphabet:
+
+for alph in array_alphabet:
     search_field.send_keys(alph)
-
-    for item in item_list:
-        div = item.find_element_by_class_name('MDICdrugs')
-        drug_name = div.get_attribute('a')
-
-        print u"""Nome: {name}""".format(name = drug_name.text)
+    search_medicine_suggention = WebDriverWait(chrome, 10).until(EC.visibility_of_all_elements_located((
+        By.XPATH, "//div[@id='MDICildruglist']//ul[@id='MDICdrugs']//li//a"
+    )))
+    for item in search_medicine_suggention:
+        print(item.text)
 
     search_field.clear()
-
-
-
-
-
